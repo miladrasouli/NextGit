cc5ebe5157218f987e5ef8eb415640cee6ab9993
zahra-hsb <zahra16toa@gmail.com>
2025-09-29 13:44:57 -0700

    feat: create button component

---
+++ components/globalComponents/Button.tsx
@@ -0,0 +1,15 @@
+import { ButtonCompType } from "@/schema/types"
+
+
+
+const Button = ({ children, className }: ButtonCompType) => {
+    return (
+        <>
+            <button className={`rounded py-2 px-3 ${className}`}>
+                {children}
+            </button>
+        </>
+    )
+}
+
+export default Button
