a82bd0f78dc76f2b59eb460c23d0505f850a3ec5
zahra-hsb <zahra16toa@gmail.com>
2025-09-29 13:45:15 -0700

    feat: create modal component

---
+++ components/globalComponents/Modal.tsx
@@ -0,0 +1,27 @@
+import Button from "./Button";
+
+
+const Modal = () => {
+    return (
+        <div className="flex items-center justify-center bg-black/30 fixed top-0 left-0 w-screen h-screen">
+            <div>
+                <div className="bg-gray-700 w-full p-3 text-white">
+                    افزودن قرارداد
+                </div>
+                <div className="bg-white p-2 border-b border-b-gray-200 ">
+                    
+                </div>
+                <div className="bg-gray-100 p-3 px-5 flex items-end justify-end gap-2">
+                    <Button className="bg-blue-700 text-white">
+                        ذخیره
+                    </Button>
+                    <Button className="bg-gray-500 text-white">
+                        انصراف
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
